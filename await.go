810ad0e1947fd09544a4f891f6307c9f2446cea5
Nick Pillitteri <nick@tshlabs.org>
2019-07-14 08:34:03 -0400

    Implement file tests

--- await.go
+++ await.go
@@ -9,8 +9,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	//"syscall"
-	//"time"
+	"time"
 )
 
 const DEFAULT_INTERVAL = 2
@@ -21,27 +20,57 @@ type filetest interface {
 
 type filefreshness struct {
 	path string
-	age int64
+	age  time.Duration
 }
 
-func (f filefreshness) statisfiled() (bool, error) {
-	return true, nil
+func newFileFreshness(path string, age time.Duration) filefreshness {
+	return filefreshness { path: path, age: age }
+}
+
+func (f filefreshness) satisfied() (bool, error) {
+	res, err := os.Stat(f.path)
+	if err != nil {
+		return false, err
+	}
+
+	mod := res.ModTime()
+	return time.Since(mod) > f.age, nil
 }
 
 type fileexists struct {
 	path string
 }
 
-func (f fileexists) statisfied() (bool, error) {
-	return true, nil
+func newFileExists(path string) fileexists {
+	return fileexists { path: path }
+}
+
+func (f fileexists) satisfied() (bool, error) {
+	if _, err := os.Stat(f.path); err == nil {
+		return true, nil
+	} else if os.IsNotExist(err) {
+		return false, nil
+	} else {
+		return false, err
+	}
 }
 
 type filenotexists struct {
 	path string
 }
 
+func newFileNotExists(path string) filenotexists {
+	return filenotexists { path: path }
+}
+
 func (f filenotexists) satisfied() (bool, error) {
-	return true, nil
+	if _, err := os.Stat(f.path); err == nil {
+		return false, nil
+	} else if os.IsNotExist(err) {
+		return true, nil
+	} else {
+		return false, err
+	}
 }
 
 func main() {
@@ -51,10 +80,23 @@ func main() {
 		flag.PrintDefaults()
 	}
 
-	//_ interval := flag.Int("interval", DEFAULT_INTERVAL, "How long between checks, in seconds")
+	interval := flag.Duration("interval", DEFAULT_INTERVAL, "How long between checks, in seconds")
 	flag.Parse()
 
 	if flag.NArg() < 1 {
-		log.Fatal("PID is required")
+		log.Fatal("File path is required")
+	}
+
+	path := flag.Arg(0)
+	check := newFileFreshness(path, 120 * time.Second)
+
+	for {
+		if done, err := check.satisfied(); done {
+			break
+		} else if err != nil {
+			log.Fatalf("Problem checking file: %s", err)
+		}
+
+		time.Sleep(*interval * time.Second)
 	}
 }
