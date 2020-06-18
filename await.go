c54d270ff5427079e7f43c29565a2379e78492b8
Nick Pillitteri <nick@tshlabs.org>
2020-06-17 20:20:03 -0400

    Copyright update

--- await.go
+++ await.go
@@ -1,6 +1,6 @@
 // Await - Wait for a file to be created/deleted/modified while blocking
 //
-// Copyright 2019 TSH Labs
+// Copyright 2019 Nick Pillitteri
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
